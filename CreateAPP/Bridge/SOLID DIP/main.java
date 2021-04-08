5ca48f7c9e840f9cd2c4b8378e05db69670b449d
Artur <64385358+KingArthurXIV@users.noreply.github.com>
2021-04-08 11:58:18 +0300

    Update main.java

--- CreateAPP/Bridge/SOLID DIP/main.java
+++ CreateAPP/Bridge/SOLID DIP/main.java
@@ -2,3 +2,20 @@ public class Main{
     public static void main(String[] Args){
     }
 }
+
+interface DAO{
+    void execute ();
+}
+
+class DataAccess implements DAO{
+    public void execute(){
+        System.out.println("Execute");
+    }
+}
+
+class Client{
+    DAO dataAccess=new DataAccess();
+    void doJob(){
+        dataAccess.execute();
+    }
+}
