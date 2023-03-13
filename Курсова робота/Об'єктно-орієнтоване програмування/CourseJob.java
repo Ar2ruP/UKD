d32996a512efa25ed68159bbd05af840d22908e4
Artur Pashnyk <64385358+Ar2ruP@users.noreply.github.com>
2023-03-13 19:47:23 +0200

    Create CourseJob.java

---
+++ Курсова робота/Об'єктно-орієнтоване програмування/CourseJob.java
@@ -0,0 +1,24 @@
+class CourseJob {
+    public static void main(String[] args) {
+
+        Queue MainQueue = new Queue();
+        MainQueue.push(15);
+        MainQueue.push(16);
+        MainQueue.push(17);
+        MainQueue.push(14);
+        MainQueue.pop();
+        MainQueue.for_each();
+        MainQueue.size();
+        MainQueue.max_min();
+        MainQueue.find(14);
+        MainQueue.equals();
+        MainQueue.get(2);
+        MainQueue.empty();
+        MainQueue.front();
+        MainQueue.back();
+        MainQueue.accumulate();
+
+
+    }
+
+}
