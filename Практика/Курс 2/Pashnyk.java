2d5c3744e49ac383b2d4cb857fcaae73076c159f
Artur Pashnyk <64385358+Ar2ruP@users.noreply.github.com>
2023-03-14 14:53:31 +0200

    Create Pashnyk.java

---
+++ Практика/Курс 2/Pashnyk.java
@@ -0,0 +1,21 @@
+//Ввести 6 чисел, знайти їх суму і середнє арифметичне.
+
+import java.util.Scanner;
+public class Pashnyk {
+    public static void main (String Args[]){
+        double sum;
+        double mid;
+        double First, Second, Third, Fourth, Fifth, Sixth;
+        Scanner Arch= new Scanner(System.in);
+        System.out.println("Введіть будь ласка 6 чисел");
+        First=Arch.nextInt();
+        Second=Arch.nextInt();
+        Third=Arch.nextInt();
+        Fourth=Arch.nextInt();
+        Fifth=Arch.nextInt();
+        Sixth=Arch.nextInt();
+        sum=First+Second+Third+Fourth+Fifth+Sixth;
+        mid=(First+Second+Third+Fourth+Fifth+Sixth)/6;
+        System.out.println("Сума всіх чисел:"+sum);
+        System.out.println("Середнє арифметичне усіх чисел:"+mid);
+    }}
