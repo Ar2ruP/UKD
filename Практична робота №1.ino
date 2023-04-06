eaca0850c7a567e01452a12d7be7a3bb7602a221
Artur Pashnyk <64385358+Ar2ruP@users.noreply.github.com>
2023-04-06 23:34:28 +0300

    Add files via upload

---
+++ Практична робота №1.ino
@@ -0,0 +1,147 @@
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH); // Буква "А"
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  digitalWrite(LED_BUILTIN, HIGH); // Буква "Р"
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  digitalWrite(LED_BUILTIN, HIGH); // Буква "Т"
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  digitalWrite(LED_BUILTIN, HIGH); // Буква "У"
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  digitalWrite(LED_BUILTIN, HIGH); // Буква "Р"
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(3000);
+  
+  
+  
+  digitalWrite(LED_BUILTIN, HIGH); // Буква "П"
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  digitalWrite(LED_BUILTIN, HIGH); // Буква "А"
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  digitalWrite(LED_BUILTIN, HIGH); // Буква "Ш"
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  digitalWrite(LED_BUILTIN, HIGH); //Буква "Н"
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  
+  digitalWrite(LED_BUILTIN, HIGH); // Буква "И"
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+  
+  digitalWrite(LED_BUILTIN, HIGH); // Буква "К"
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(300); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(1000); 
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  
+}
