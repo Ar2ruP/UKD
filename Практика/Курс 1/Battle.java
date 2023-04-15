5dd664f041e0b4c9e33ad92a24c28bda7fb8e0e3
Artur Pashnyk <64385358+Ar2ruP@users.noreply.github.com>
2023-04-15 23:18:45 +0300

    Update and rename Calendar.java to Battle.java

--- Практика/Курс 1/Battle.java
+++ Практика/Курс 1/Battle.java
@@ -1,7 +1,7 @@
 /*Скласти програму,  яка б повiдомляла,  що битва пiд Берестечком вiдбулася в 1649р.,  запитувала,  який тепер рiк,  i друкувала, скiльки рокiв пройшло з того часу.*/
 
 import java.util.Scanner;
-public class Calendar {
+public class Battle {
     public static void main (String Args[]){
         int b=1649,c;
         Scanner Arch =new Scanner(System.in);
