8dcf8cd183d2730f7d852f64ad4af9fffabaeb24
Artur Pashnyk <64385358+Ar2ruP@users.noreply.github.com>
2023-04-15 23:14:06 +0300

    Create Typescript.java

---
+++ Практика/Курс 1/Typescript.java
@@ -0,0 +1,19 @@
+/*Скласти програму для виводу шапки i одного рядка машинограми*/
+
+import java.util.Scanner;
+public class Typescript {
+    public static void main (String Args[]){
+        int District, Shop, SequenceNumber;
+        double Issued;
+        Scanner Arch=new Scanner(System.in);
+        System.out.println("Введіть порядковий номер:");
+        SequenceNumber=Arch.nextInt();
+        System.out.println("Введіть номер дільниці:");
+        District=Arch.nextInt();
+        System.out.println("Введіть номер цеху:");
+        Shop=Arch.nextInt();
+        System.out.println("Введіть видану суму:");
+        Issued=Arch.nextInt();
+        System.out.print("№/ПП:"+SequenceNumber+"| Дільниця:"+District+"| Цех:"+Shop+"| Видано:"+Issued);
+  }
+}
