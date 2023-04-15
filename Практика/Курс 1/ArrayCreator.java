2d054643b4545e8d6290768c836191d6169cf90b
Artur Pashnyk <64385358+Ar2ruP@users.noreply.github.com>
2023-04-15 23:30:04 +0300

    Create ArrayCreator.java

---
+++ Практика/Курс 1/ArrayCreator.java
@@ -0,0 +1,18 @@
+/*Органiзувавши керований ввiд, ввести ряд чисел i сформувати масив з тих, якi задовольняють певну умову (дiйснi числа, бiльшi 1 i меншi 7). Перебачити довжину масиву не бiльше 10 елементiв. */
+
+import java.util.Scanner;
+public class ArrayCreator {
+    public static void main(String[] args) {
+        int []a=new int[10];
+        int i;
+        Scanner King=new Scanner(System.in);
+        System.out.println("Введіть 10 чисел");
+        for(i=0;i<a.length;i++)
+            a[i]=King.nextInt();
+        System.out.print("{");
+        for(i=0;i<10;i++)
+            if(a[i]>1 && a[i]<7)
+                System.out.print(" "+a[i]);
+            System.out.print(" }");
+    }
+}
