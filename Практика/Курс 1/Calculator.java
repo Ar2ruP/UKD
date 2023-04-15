f4e958898641dc8b5b250d8aeb174b2070c13c8e
Artur Pashnyk <64385358+Ar2ruP@users.noreply.github.com>
2023-04-15 23:11:53 +0300

    Create Calculator.java

---
+++ Практика/Курс 1/Calculator.java
@@ -0,0 +1,16 @@
+/*Скласти програму,  яка вводила б два цiлi числа, знаходила iх добуток,  рiзницю i виводила результат на екран.*/
+import java.util.Scanner;
+public class Calculator {
+    public static void main (String Args[]){
+        int dif, dob;
+        Scanner Arch =new Scanner(System.in);
+        System.out.println("Введіть перше число");
+        int a=Arch.nextInt();
+        System.out.println("Введіть Друге число");
+        int b=Arch.nextInt();
+        dob=a*b;
+        dif=a-b;
+        System.out.println("Добуток першого і другого числа буде дорівнювати " +dob);
+        System.out.println("Різниця першого і другого числа буде дорівнювати " +dif);
+    }
+}
