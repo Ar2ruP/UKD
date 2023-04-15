1479d6c04c5d8d085ca6f7a008053503082091fe
Artur Pashnyk <64385358+Ar2ruP@users.noreply.github.com>
2023-04-16 01:21:14 +0300

    Create Lab3.cpp

---
+++ Основи програмування/Курс 2/Lab3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+  const float π=3.14;
+  float r=4, P, S;
+  P=2*π*r;
+  S=π*r*r;
+  cout<<"Периметр квіткової клумби"<<P;
+  cout<<"Площа квіткової клумби"<<S;
+  return 0;
+}
