1928cf19879bf2c9971b0afa2a129ba7903e2fc7
Artur Pashnyk <64385358+Ar2ruP@users.noreply.github.com>
2023-04-15 23:10:05 +0300

    Create Calendar.java

---
+++ Практика/Курс 1/Calendar.java
@@ -0,0 +1,13 @@
+/*Скласти програму,  яка б повiдомляла,  що битва пiд Берестечком вiдбулася в 1649р.,  запитувала,  який тепер рiк,  i друкувала, скiльки рокiв пройшло з того часу.*/
+
+import java.util.Scanner;
+public class Calendar {
+    public static void main (String Args[]){
+        int b=1649,c;
+        Scanner Arch =new Scanner(System.in);
+        System.out.println("Який сьогодні рік?");
+        int a=Arch.nextInt();
+        c=a-b;
+        System.out.println("Битва під Берестечком відбулася "+c+" років тому");
+    }
+}
