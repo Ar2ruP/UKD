1e109d51d1610da8531db9fe2e187c216951432b
Artur Pashnyk <64385358+Ar2ruP@users.noreply.github.com>
2023-04-16 03:27:45 +0300

    Create Lab11.cpp

---
+++ Основи програмування/Курс 2/Lab11.cpp
@@ -0,0 +1,21 @@
+/*Ввести масив що складаєьбся з 20 елементів цілого типу. Визначити яких елементів більше парних чи непарних? */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+int main(){
+  int even=0, odd=0, i=0;
+  int a[20];
+  for(i=0;i<20;i++)
+    a[i]=rand()%20;
+  for(i=0;i<20;i++){
+    if(a[i]%2=0)
+      even++;
+    if(a[i]%2!=0)
+      odd++;
+  }
+  if(even>odd)
+    cout<<"Парних чисел більше";
+  else cout<<"Непарних чисел більше";
+}
